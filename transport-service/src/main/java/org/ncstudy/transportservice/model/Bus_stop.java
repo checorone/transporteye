b13a06735d597603bf05057d94253d9ff9e5ca76
Tima <P_timofei@mail.ru>
2019-05-19 18:24:50 +0300

    -added Validation Entity

--- transport-service/src/main/java/org/ncstudy/transportservice/model/Bus_stop.java
+++ transport-service/src/main/java/org/ncstudy/transportservice/model/Bus_stop.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 public class Bus_stop {
     @Id
     @Column(name = "bus_stop_id", nullable = false)
-    private Integer bus_stop_id;
+    private String bus_stop_id;
 
     @Column(name = "bus_stop_name", nullable = false)
     private String bus_stop_name;
@@ -25,18 +25,18 @@ public class Bus_stop {
     public Bus_stop() {
     }
 
-    public Bus_stop(Integer bus_stop_id, String bus_stop_name, String latitude, String longitude) {
+    public Bus_stop(String  bus_stop_id, String bus_stop_name, String latitude, String longitude) {
         this.bus_stop_id = bus_stop_id;
         this.bus_stop_name = bus_stop_name;
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
-    public Integer getBus_stop_id() {
+    public String  getBus_stop_id() {
         return bus_stop_id;
     }
 
-    public void setBus_stop_id(Integer bus_stop_id) {
+    public void setBus_stop_id(String  bus_stop_id) {
         this.bus_stop_id = bus_stop_id;
     }
 

b847207366ec575f0a6caa8b836f202e8e308a3a
checorone <checordog@gmail.com>
2019-06-07 18:10:06 +0300

    Removed frontend from maven build

--- frontend-service/NEW_ANGULAR/src/app/shared/shared.module.ts
+++ frontend-service/NEW_ANGULAR/src/app/shared/shared.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-// import { SuccessDialogComponent } from './dialogs/success-dialog/success-dialog.component';
-// import { ErrorDialogComponent } from './dialogs/error-dialog/error-dialog.component';
 
 @NgModule({
   imports: [
@@ -14,16 +12,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   exports: [
     MaterialModule,
     FlexLayoutModule
-    // SuccessDialogComponent,
-    // ErrorDialogComponent
   ],
   declarations: [
-    // SuccessDialogComponent, 
-    // ErrorDialogComponent
   ],
   entryComponents: [
-    // SuccessDialogComponent,
-    // ErrorDialogComponent
   ]
 })
 export class SharedModule { }

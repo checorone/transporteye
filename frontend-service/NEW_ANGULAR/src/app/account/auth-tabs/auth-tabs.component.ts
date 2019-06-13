57e524e1c294c77ecafa048821f968bb5938da15
Anshin-nc <a21mor@mail.ru>
2019-06-13 03:24:01 +0300

    minor changes

---
+++ frontend-service/NEW_ANGULAR/src/app/account/auth-tabs/auth-tabs.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-auth-tabs',
+  templateUrl: './auth-tabs.component.html',
+  styleUrls: ['./auth-tabs.component.less']
+})
+export class AuthTabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
